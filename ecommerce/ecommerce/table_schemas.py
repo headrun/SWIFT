e3428a25127bd8409da8d160785588a61f2b0b46
charan <charan@headrun.com>
2020-05-16 16:23:07 +0530

    #56 adding schema changes

--- ecommerce/ecommerce/table_schemas.py
+++ ecommerce/ecommerce/table_schemas.py
@@ -17,39 +17,39 @@ CREATE TABLE `#CRAWL-TABLE#` (
 
 PRODUCTS_INFO_TABLE_CREATE_QUERY="""
 CREATE TABLE `products_info` (
-  `hd_id` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `source` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `sku` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `size` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `title` varchar(512) COLLATE utf8_unicode_ci NOT NULL,
-  `descripion` text COLLATE utf8_unicode_ci,
-  `specs` text COLLATE utf8_unicode_ci,
-  `image_url` text COLLATE utf8_unicode_ci,
-  `reference_url` text COLLATE utf8_unicode_ci,
-  `aux_info` text COLLATE utf8_unicode_ci,
+  `hd_id` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `source` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `sku` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `size` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `title` varchar(512) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `descripion` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+  `specs` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+  `image_url` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+  `reference_url` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+  `aux_info` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `created_at` datetime NOT NULL,
-  PRIMARY KEY (`hd_id`),
+  PRIMARY KEY (`hd_id`,`created_at`),
   UNIQUE KEY `source` (`source`,`sku`,`size`,`created_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
 """
 
 PRODUCSTS_INSIGHTS_TABLE_CREATE_QUERY="""
 CREATE TABLE `products_insights` (
-  `hd_id` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `source` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `sku` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `size` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `category` text COLLATE utf8_unicode_ci NOT NULL,
-  `sub_category` text COLLATE utf8_unicode_ci NOT NULL,
-  `brand` text COLLATE utf8_unicode_ci NOT NULL,
+  `hd_id` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `source` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `sku` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `size` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `category` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `sub_category` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `brand` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `ratings_count` int DEFAULT '0',
   `reviews_count` int DEFAULT '0',
-  `mrp` float DEFAULT '0.0',
-  `selling_price` float DEFAULT '0.0',
-  `discount_percentage` float DEFAULT '0.0',
+  `mrp` float DEFAULT '0',
+  `selling_price` float DEFAULT '0',
+  `discount_percentage` float DEFAULT '0',
   `is_available` int DEFAULT '0',
   `created_at` datetime NOT NULL,
-  PRIMARY KEY (`hd_id`),
+  PRIMARY KEY (`hd_id`,`created_at`),
   UNIQUE KEY `source` (`source`,`sku`,`size`,`created_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
 """
