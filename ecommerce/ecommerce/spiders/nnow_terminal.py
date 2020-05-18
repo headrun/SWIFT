a087f8d98b3de702505ce9b6928b619346308357
Preethi266 <preethi@headrun.com>
2020-05-18 10:58:22 +0530

    #50 nnnow terminal spider

---
+++ ecommerce/ecommerce/spiders/nnow_terminal.py
@@ -0,0 +1,62 @@
+import json
+from ecommerce.common_utils import *
+import re
+from ecommerce.items import InsightItem, MetaItem
+
+class NowSpider(EcommSpider):
+    name = "nnnow_fashion_terminal"
+
+    def parse(self,response):
+        datas = response.xpath('//script[contains(text(),"window.DATA")]/text()').extract()
+        for data in datas:
+            total_data = re.findall('window.DATA= (.*)',data)
+            for total in total_data:
+                source = 'nnnow'
+                data1 = json.loads(total)
+                style = data1.get('ProductStore',{}).get('PdpData',{}).get('mainStyle','')
+                product_id = style.get('styleId','')
+                brandname = style.get('brandName','')
+                name = style.get('name','')
+                status = style.get('status','')
+                url = style.get('url','')
+                category = style.get('gender','')
+                availability = style.get('inStock','')
+                title = style.get('finerDetails',{}).get('compositionAndCare',{}).get('title','')
+                details = ','.join(style.get('finerDetails',{}).get('compositionAndCare',{}).get('list',''))
+                description = title+', '+details
+                specs_title = style.get('finerDetails',{}).get('specs',{}).get('title','')
+                specs_details = ','.join(style.get('finerDetails',{}).get('specs',{}).get('list',''))
+                specs = specs_title+', '+specs_details
+                aux_info = {'product_id': product_id, 'json_page': response.url}
+                tags = style.get('productTags',[])
+                for tag in tags:
+                    tagtext = tag.get('tagText','')
+                images = style.get('images',[])
+                for image in images:
+                    large_image = image.get('large','')
+                skus = style.get('skus',[])
+                for sku in skus:
+                    size = sku.get('size','')
+                    skuid = sku.get('skuId','')
+                    quantity = sku.get('maxQuantityDisplayed','')
+                    mrp = sku.get('mrp','')
+                    price = sku.get('price','')
+                    discount = sku.get('discountInPercentage','')
+                    sellable = sku.get('sellableQuantity','')
+                    insight_item = InsightItem()
+                    hd_id = encode_md5('%s%s%s' % (source, str(skuid), size))
+                    insight_item.update({
+                        'hd_id': hd_id, 'source': source, 'sku': sku, 'size': size, 'category':category,
+                        'sub_category': '', 'brand': brandname, 'ratings_count': '',
+                        'reviews_count': '', 'mrp': mrp, 'selling_price': price,
+                        'discount_percentage': discount,'is_available': availability
+                    })
+                    yield insight_item    
+                    
+                    meta_item = MetaItem()
+                    meta_item.update({
+                        'hd_id': hd_id, 'source': source, 'sku': sku, 'size': size, 'title': name,
+                        'descripion': description, 'specs':specs, 'image_url': large_image, 
+                        'reference_url': response.url, 'aux_info': json.dumps(aux_info)
+                    })
+                    yield meta_item    
