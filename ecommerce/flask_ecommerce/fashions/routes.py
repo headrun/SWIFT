62550d0533aec5f23907d52f9e07dcb927e59be7
mohanav <mohana@headrun.com>
2020-05-25 17:24:19 +0530

    #Added UI for ecommerce site along with icon, rating and pagination

--- ecommerce/flask_ecommerce/fashions/routes.py
+++ ecommerce/flask_ecommerce/fashions/routes.py
@@ -14,24 +14,36 @@ mysql = MySQL(app)
 @app.route('/')
 def home():
 	cur = mysql.connection.cursor()
-	today_date = str(datetime.datetime.now().date())
-	cur.execute("SELECT t1.image_url,t1.title,t2.mrp,t2.selling_price,t2.brand FROM (SELECT * FROM products_info limit 50) t1 join products_insights t2 on t1.hd_id = t2.hd_id")
-	# cur.execute("select * from products_info where created_at >= STR_TO_DATE('2013-07-11','%Y-%m-%d') and created_at <= STR_TO_DATE('2013-07-11','%Y-%m-%d') limit 1;")
-	row_headers=[x[0] for x in cur.description]
-	results = cur.fetchall()
-	json_data=[]
-	for result in results:
-	    json_data.append(dict(zip(row_headers,result)))
-	products = json.dumps(json_data)
-	cur.close()
-	return render_template('details.html',products=json.loads(products))
-
+	return render_template('details.html')
 @app.route('/search')
 def data():
+	start_date = request.args.get('startdate')
+	end_date = request.args.get('enddate')
+	source = request.args.get('source')
+	sortBy = request.args.get('sortBy')
+	page_num = request.args.get('page_num',1)
+	limit=20
+	offset=0
+	print("page_num, limit",page_num,limit)
+	if(page_num==0 or page_num==1):
+		offset=0
+	else:
+		offset=(int(page_num)-1)*limit
 	cur = mysql.connection.cursor()
-	today_date = str(datetime.datetime.now().date())
-	cur.execute("SELECT t1.image_url,t1.title,t2.mrp,t2.selling_price,t2.brand FROM (SELECT * FROM products_info limit 50) t1 join products_insights t2 on t1.hd_id = t2.hd_id")
-	# cur.execute("select * from products_info where created_at >= STR_TO_DATE('2013-07-11','%Y-%m-%d') and created_at <= STR_TO_DATE('2013-07-11','%Y-%m-%d') limit 1;")
+	if sortBy == "popularity":
+		if source == "myntra":
+			cur.execute("select DISTINCT t1.hd_id,t2.brand,t2.ratings_count,t2.source,t2.selling_price,t2.mrp,t1.image_url,t1.title from products_insights t2,products_info t1 where t1.hd_id = t2.hd_id and t1.created_at >= STR_TO_DATE(%s,'%%Y-%%m-%%d') and t1.created_at <= STR_TO_DATE(%s,'%%Y-%%m-%%d') and t1.source='myntra' ORDER BY t2.ratings_count DESC limit %s offset %s;",[start_date,end_date,limit,offset])
+		elif source == "nnnow":
+			cur.execute("select DISTINCT t1.hd_id,t2.brand,t2.ratings_count,t2.source,t2.selling_price,t2.mrp,t1.image_url,t1.title from products_insights t2,products_info t1 where t1.hd_id = t2.hd_id and t1.created_at >= STR_TO_DATE(%s,'%%Y-%%m-%%d') and t1.created_at <= STR_TO_DATE(%s,'%%Y-%%m-%%d') and t1.source='nnnow' ORDER BY t2.ratings_count DESC limit %s offset %s;",[start_date,end_date,limit,offset])
+		else:
+			cur.execute("select DISTINCT t1.hd_id,t2.brand,t2.ratings_count,t2.source,t2.selling_price,t2.mrp,t1.image_url,t1.title from products_insights t2,products_info t1 where t1.hd_id = t2.hd_id and t1.created_at >= STR_TO_DATE(%s,'%%Y-%%m-%%d') and t1.created_at <= STR_TO_DATE(%s,'%%Y-%%m-%%d') ORDER BY t2.ratings_count DESC limit %s offset %s;",[start_date,end_date,limit,offset])
+	else:
+		if source == "myntra":
+			cur.execute("select DISTINCT t1.hd_id,t2.brand,t2.ratings_count,t2.source,t2.selling_price,t2.mrp,t1.image_url,t1.title from products_insights t2,products_info t1 where t1.hd_id = t2.hd_id and t1.created_at >= STR_TO_DATE(%s,'%%Y-%%m-%%d') and t1.created_at <= STR_TO_DATE(%s,'%%Y-%%m-%%d') and t1.source='myntra' limit %s offset %s;",[start_date,end_date,limit,offset])
+		elif source == "nnnow":
+			cur.execute("select DISTINCT t1.hd_id,t2.brand,t2.ratings_count,t2.source,t2.selling_price,t2.mrp,t1.image_url,t1.title from products_insights t2,products_info t1 where t1.hd_id = t2.hd_id and t1.created_at >= STR_TO_DATE(%s,'%%Y-%%m-%%d') and t1.created_at <= STR_TO_DATE(%s,'%%Y-%%m-%%d') and t1.source='nnnow' limit %s offset %s;",[start_date,end_date,limit,offset])
+		else:
+			cur.execute("select DISTINCT t1.hd_id,t2.brand,t2.ratings_count,t2.source,t2.selling_price,t2.mrp,t1.image_url,t1.title from products_insights t2,products_info t1 where t1.hd_id = t2.hd_id and t1.created_at >= STR_TO_DATE(%s,'%%Y-%%m-%%d') and t1.created_at <= STR_TO_DATE(%s,'%%Y-%%m-%%d') limit %s offset %s;",[start_date,end_date,limit,offset])
 	row_headers=[x[0] for x in cur.description]
 	results = cur.fetchall()
 	json_data=[]
@@ -39,5 +51,4 @@ def data():
 		json_data.append(dict(zip(row_headers,result)))
 	products = json.dumps(json_data)
 	cur.close()
-	return render_template('details.html',products=json.loads(products))
-
+	return products
