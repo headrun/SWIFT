6ba9bbcc67c100e5a46de75be4f149f20dabecce
Preethi266 <preethi@headrun.com>
2020-06-08 16:49:30 +0530

    #88 added the reviwes and rating_count

--- ecommerce/ecommerce/spiders/costco_browse.py
+++ ecommerce/ecommerce/spiders/costco_browse.py
@@ -8,7 +8,7 @@ class CostcoSpider(EcommSpider):
     def __init__(self, *args, **kwargs):
         super(CostcoSpider, self).__init__(*args, **kwargs)
         self.category_array = ['mens-outerwear.html', 'mens-activewear.html', 'pants.html', 'mens-shirts.html', 'mens-sweaters.html', 'mens-suits.html', 'mens-sleepwear.html', 'mens-shorts.html', 'mens-swimwear.html']
-        self.headers = headers = {
+        self.headers = {
             'authority': 'www.costco.com',
             'pragma': 'no-cache',
             'cache-control': 'no-cache',
@@ -27,26 +27,31 @@ class CostcoSpider(EcommSpider):
 
     def parse(self, response):
         urls = response.xpath('//p[@class="description"]//a//@href').extract()
-        for url in urls:
-            yield Request(url, callback=self.parse_data, headers=self.headers, meta={'category':response.url})
+        reviews = response.xpath('//meta[@itemprop="reviewCount"]/@content').extract()
+        ratings = response.xpath('//meta[@itemprop="ratingValue"]//@content').extract()
+        availabilities = response.xpath('//input[@id="in_Stock"]//@value').extract()
+        for url, review, rating, availability in zip(urls, reviews, ratings, availabilities):
+            meta = {'category':response.url, 'review':review, 'rating':rating, 'availability':availability}
+            yield Request(url, callback=self.parse_data, headers=self.headers, meta=meta)
 
     def parse_data(self, response):
-        category = response.meta['category']
         source = 'costco'
+        reviews = response.meta['review']
+        rating = response.meta['rating']
+        category = response.meta['category']
+        availability = response.meta['availability']
         item = ''.join(response.xpath('//p[@class="member-only"]//text()').extract())
         brandname = ''.join(response.xpath('//div[@itemprop="brand"]//text()').extract())
         product_name = ''.join(response.xpath('//h1[@itemprop="name"]//text()').extract()).replace('\t', '')
-        colours = ' '.join(response.xpath('//div[@class="style-select  form-group"]//text()').extract()).replace('\r\n\t', '').replace('\t', '').strip()
-        description = ','.join(response.xpath('//div[@class="product-info-description"]//ul/li/text()').extract()).replace('\xa0', '').replace('\t', '')
+        description = ','.join(response.xpath('//ul[@class="pdp-features"]//li//text()').extract()).replace('\n', '')
         currency_ = ''.join(response.xpath('//meta[@property="product:price:currency"]//@content').extract())
-        currency = response.xpath('//div[@class="pull-right"]/span/text()').extract()[1]
         mrp = ''.join(response.xpath('//meta[@property="product:price:amount"]//@content').extract())
         try:
             category_ = category.split('/')[-1].split('-')[0]
             sub_category = category.split('/')[-1].split('-')[1]
         except:
-            category_ = ''
-            sub_category = '' 
+            category_ = 'mens'
+            sub_category = category_ 
         product_id = response.url.split('.')[-2]
         aux_info = {'product_id': product_id, 'json_page': response.url}
         image_url = ''.join(response.xpath('//img[@class="img-responsive"]//@src').extract())
@@ -57,13 +62,9 @@ class CostcoSpider(EcommSpider):
             sku_id = sku
             hd_id = encode_md5('%s%s%s' % (source, str(sku_id), size))
             insight_item = InsightItem()
-            insight_item.update({'hd_id': hd_id, 'source': source, 'sku': sku_id, 'size': size, 'category':category_, 'sub_category': sub_category, 'brand': brandname, 'ratings_count': '', 'reviews_count': '', 'mrp':mrp, 'selling_price':'', 'discount_percentage':'', 'is_available': ''})
+            insight_item.update({'hd_id': hd_id, 'source': source, 'sku': sku_id, 'size': size, 'category':category_, 'sub_category': sub_category, 'brand': brandname, 'ratings_count':'', 'reviews_count': reviews, 'mrp':mrp, 'currency':currency_, 'selling_price':'', 'discount_percentage':'', 'is_available': availability})
             yield insight_item
+
             meta_item = MetaItem()
-            meta_item.update({'hd_id': hd_id, 'source': source, 'sku': sku_id, 'web_id':product_id, 'size': size, 'title': product_name, 'category':category_, 'sub_category':sub_category, 'brand':brandname, 'rating':'', 'ratings_count':'', 'reviews_count':'', 'mrp':mrp, 'selling_price':'', 'discount_percentage':'', 'is_available':'', 'descripion': description, 'specs':'', 'image_url':image_url, 'reference_url': response.url, 'aux_info': json.dumps(aux_info)})            
+            meta_item.update({'hd_id': hd_id, 'source': source, 'sku': sku_id, 'web_id':product_id, 'size': size, 'title': product_name, 'category':category_, 'sub_category':sub_category, 'brand':brandname, 'rating':rating, 'ratings_count':'', 'reviews_count':reviews, 'mrp':mrp, 'currency':currency_, 'selling_price':'', 'discount_percentage':'', 'is_available': availability, 'descripion': description, 'specs':'', 'image_url':image_url, 'reference_url': response.url, 'aux_info': json.dumps(aux_info)})            
             yield meta_item
-
-
-            
-       
-        
