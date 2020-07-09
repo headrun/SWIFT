8aded1ecfd52c88cdd00bc46045e80b725d13eda
anandhurajan <anandhu@headrun.com>
2020-07-09 14:58:37 +0530

    #129 Added Segment category in BSE

--- bse/bse/spiders/bse_xpath.py
+++ bse/bse/spiders/bse_xpath.py
@@ -1,10 +1,11 @@
 import json
+import datetime
 from ast import literal_eval
 from urllib.parse import parse_qs, urlparse, urlencode
-from MySQLdb import connect
-from pandas import read_sql, DataFrame
-from pydispatch import dispatcher
 from collections import OrderedDict
+from MySQLdb import connect
+from pandas import DataFrame
+from pydispatch import dispatcher
 from scrapy import signals
 from scrapy.spiders import Spider
 from scrapy.http import Request, FormRequest
@@ -43,88 +44,112 @@ class Bse(Spider):
         for code in self.crawl_list:
             params = [('scripcode', code)]
 
-            financials_url = 'https://api.bseindia.com/BseIndiaAPI/api/GetReportNewFor_Result/w?%s' % urlencode(params)
+            financials_url = 'https://api.bseindia.com/BseIndiaAPI/api/GetReportNewFor_Result/w?%s' % urlencode(
+                params)
             yield Request(financials_url, callback=self.parse_financials, headers=headers)
 
             params.append(('qtrid', ''))
-            shares_url = 'https://api.bseindia.com/BseIndiaAPI/api/shpSecSummery_New/w?%s' % urlencode(params)
+            shares_url = 'https://api.bseindia.com/BseIndiaAPI/api/shpSecSummery_New/w?%s' % urlencode(
+                params)
             yield Request(shares_url, callback=self.parse_shares, headers=headers)
 
             notice_url = 'https://www.bseindia.com/markets/MarketInfo/NoticesCirculars.aspx?txtscripcd=%s' % code
             yield Request(notice_url, callback=self.parse_notices, headers=headers)
-        self.cursor.execute("update bse_crawl set crawl_status = 1 where security_code = '{0}'".format(code))
-    
+
     def parse_financials(self, response):
         res_url = str(response.url)
         scrip_code = parse_qs(urlparse(res_url).query)['scripcode'][0]
         body = json.loads(response.body)
-        sel = Selector(text=body.get('QtlyinCr', ''))
-        category = 'results_qrt'
-        sub_heading = sel.xpath(
-            '//tbody//tr//td[@align="left"]//text()').extract()
-        main_heading = sel.xpath(
-            '//body//thead//tr//td[@class="tableheading"]//text()').extract()
-        col_values = sel.xpath(
-            '//tbody//tr//td[@align="right"]//text()').extract()
+        terms = ['QtlyinCr', 'AnninCr']
+        for term in terms:
+            sel = Selector(text=body.get(term, ''))
+            if term in 'QtlyinCr':
+                category = 'results_qrt'
+            else:
+                category = 'results_annual'
+            sub_heading = sel.xpath(
+                '//tbody//tr//td[@align="left"]//text()').extract()
+            main_heading = sel.xpath(
+                '//body//thead//tr//td[@class="tableheading"]//text()').extract()
+            col_values = sel.xpath(
+                '//tbody//tr//td[@align="right"]//text()').extract()
+            standalone = sel.xpath(
+                '//tbody/tr/td/a[contains(text(),"Standalone")]/@href').extract()
+            consolidated = sel.xpath(
+                '//tbody/tr/td/a[contains(text(),"Consolidated")]/@href').extract()
+            segment = sel.xpath(
+                '//tbody/tr/td/a[contains(text(),"Segment")]/@href').extract()
+            if term in 'QtlyinCr':
+                table = 'result_qrt_trends'
+            else:
+                table = 'result_annual_trends'
+            for urls in standalone:
+                standalone_url = 'https://www.bseindia.com' + urls
+                yield Request(standalone_url, callback=self.segment_parse, meta={'tbl_name': table, 'type': 'standalone'})
+            for urls in consolidated:
+                consolidated_url = 'https://www.bseindia.com' + urls
+                yield Request(consolidated_url, callback=self.segment_parse, meta={'tbl_name': table, 'type': 'consolidated'})
+            for urls in segment:
+                segment_url = 'https://www.bseindia.com' + urls
+                yield Request(segment_url, callback=self.segment_parse, meta={'tbl_name': table, 'type': 'segment'})
 
-        data = DataFrame()
-        temp = []
-        start = 0
-        for i in range(len(main_heading)-1, len(col_values)+1, len(main_heading)-1):
-            temp.append(col_values[start:i])
-            start = i
+            data = DataFrame()
+            temp = []
+            start = 0
+            for i in range(len(main_heading)-1, len(col_values)+1, len(main_heading)-1):
+                temp.append(col_values[start:i])
+                start = i
+            data = DataFrame([temp[i] for i in range(len(temp))],
+                             columns=main_heading[1:])
+            data[sub_heading[0]] = sub_heading[1:]
+            data = [data.columns.tolist()] + data.values.tolist()
+            lis_ = []
+            for row in data[1:]:
+                ds = OrderedDict()
+                for idx, val in enumerate(row):
+                    ds[data[0][idx]] = val
+                lis_.append(ds)
+            output_dict = {category: lis_}
+            query = "insert ignore into %s (scrip_code, json_value, created_at, updated_at) values('%s', '%s', now(), now())"
+            self.cursor.execute(
+                query % (category, scrip_code, json.dumps(output_dict)))
+            self.conn.commit()
 
-        data = DataFrame([temp[i] for i in range(len(temp))],
-                         columns=main_heading[1:])
-
-        data[sub_heading[0]] = sub_heading[1:]
-        data = [data.columns.tolist()] + data.values.tolist()
-        lis_ = []
-        for row in data[1:]:
-            ds = OrderedDict()
-            for idx, val in enumerate(row):
-                ds[data[0][idx]] = val
-            lis_.append(ds)
-        output_dict = {category: lis_}
-        query = "insert ignore into results_qrt (scrip_code, json_value, created_at, updated_at) values('%s', '%s', now(), now())"
-        self.cursor.execute(query % (scrip_code, json.dumps(output_dict)))
-        self.conn.commit()
-
-        body = json.loads(response.body)
-        sel = Selector(text=body.get('AnninCr', ''))
-        category = 'results_annual'
-        sub_heading = sel.xpath(
-            '//tbody//tr//td[@align="left"]//text()').extract()
-        main_heading = sel.xpath(
-            '//body//thead//tr//td[@class="tableheading"]//text()').extract()
-        col_values = sel.xpath(
-            '//tbody//tr//td[@align="right"]//text()').extract()
-        data = DataFrame()
-        temp = []
-        start = 0
-        for i in range(len(main_heading)-1, len(col_values)+1, len(main_heading)-1):
-            temp.append(col_values[start:i])
-            start = i
-
-        data = DataFrame([temp[i] for i in range(
-            len(temp))], columns=main_heading[1:])
-        data[sub_heading[0]] = sub_heading[1:]
-        data = [data.columns.tolist()] + data.values.tolist()
-        lis_ = []
-        for row in data[1:]:
-            ds = OrderedDict()
-            for idx, val in enumerate(row):
-                ds[data[0][idx]] = val
-            lis_.append(ds)
-        output_dict = {category: lis_}
-        query = "insert ignore into results_annual (scrip_code, json_value, created_at, updated_at) values('%s', '%s', now(), now())"
-        self.cursor.execute(query % (scrip_code, json.dumps(output_dict)))
+    def segment_parse(self, response):
+        res_url = str(response.url)
+        tbl_name = response.meta['tbl_name']
+        res_type = response.meta['type']
+        scrip_code = parse_qs(urlparse(res_url).query)['Code'][0]
+        res = Selector(response)
+        trs = res.xpath('//table[@id="ContentPlaceHolder1_tbl_typeID"]/tr')
+        fin_out = {}
+        for tr in trs:
+            tds = tr.xpath('td/text()').extract()
+            if len(tds) == 2:
+                key, value = tds
+                fin_out[key] = value
+            elif len(tds) == 1:
+                fin_out[tds[0]] = ''
+        dt = fin_out['Date End']
+        try:
+            end_date = datetime.datetime.strptime(
+                dt, '%d-%b-%y').strftime('%Y-%m-%d')
+        except:
+            pass
+        try:
+            end_date = datetime.datetime.strptime(
+                dt, '%d-%b-%Y').strftime('%Y-%m-%d')
+        except:
+            pass
+        query = "insert into %s (scrip_code, res_type, end_date, json_value, created_at, updated_at) values('%s', '%s', '%s', '%s', now(), now()) on duplicate key update json_value = '%s'"
+        self.cursor.execute(query % (tbl_name, scrip_code, res_type,
+                                     end_date, json.dumps(fin_out), json.dumps(fin_out)))
         self.conn.commit()
 
     def parse_shares(self, response):
         res_url = str(response.url)
         scrip_code = parse_qs(urlparse(res_url).query)['scripcode'][0]
-        category='shareholding_pattern'
+        category = 'shareholding_pattern'
         fin_values = []
         body = json.loads(response.body)
         sel = Selector(text=body.get('Data', ''))
@@ -178,7 +203,7 @@ class Bse(Spider):
                       'segmentname': segmentname, 'categoryname': categoryname, 'departmentname': departmentname}
             temp = DataFrame([fields])
             data1 = data1.append(temp)
-        
+
         page_no = response.meta.get('page_no', '2')
         data = {
             '__EVENTTARGET': 'ctl00$ContentPlaceHolder1$GridView2',
@@ -210,11 +235,11 @@ class Bse(Spider):
         yield FormRequest(response.url, formdata=data, callback=self.parse_notices, meta=meta)
         data1['scrip_code'] = scrip_code
         column_names = data1.columns.to_list()
-        column_names = column_names + [ 'created_at', 'modified_at']
+        column_names = column_names + ['created_at', 'modified_at']
         column_names = [item.lower() for item in column_names if item]
         for i in range(len(data1)):
             column_values = tuple(data1.iloc[i].values)
-            values_ = ['%s'] * len(column_names)
-            query = "insert ignore into  notice ({0}) values ({1}, now(), now()) on duplicate key update modified_at = now()".format(','.join(column_names), (('%s,')*(len(column_names)-2)).strip(','))
+            query = "insert ignore into  notice ({0}) values ({1}, now(), now()) on duplicate key update modified_at = now()".format(
+                ','.join(column_names), (('%s,')*(len(column_names)-2)).strip(','))
             self.cursor.execute(query, column_values)
             self.conn.commit()
